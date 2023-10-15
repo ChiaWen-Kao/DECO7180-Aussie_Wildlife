5ceb4fbdab4c8fef6d7ff5777a18d6ba2e278437
LiChihTseng <lichih.tseng@uqconnect.edu.au>
2023-10-15 23:34:20 +1000

    Update speech button on every page

---
+++ src/js/choose.js
@@ -0,0 +1,42 @@
+let currentImageIndex = 0; // Index of the current image to be displayed
+
+let currentUtterance = null; // Track the currently speaking utterance
+
+
+//----------------------------------------------------------------------------------------------
+//using [SpeechSynthesisUtterance] interface of the [Web Speech API]
+
+
+function speakContent(text) {
+    let utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    utterance.pitch = 1.2;
+    utterance.rate = 0.8;
+    let voices = window.speechSynthesis.getVoices();
+    let selectedVoice = voices.find(voice => voice.name === "Google UK English Male");
+    
+    if (selectedVoice) {
+        utterance.voice = selectedVoice;
+    } else {
+        // utterance.voice = voices[0];
+        console.log('123');
+    }
+    
+    currentUtterance = utterance; // Update the currently speaking utterance
+    window.speechSynthesis.speak(utterance);
+}
+
+//----------------------------------------------------------------------------------------------
+
+const contextElement = document.querySelector('.context');
+let currentContentIndex = 0;
+
+const playSoundsButton = document.getElementById('next');
+
+playSoundsButton.addEventListener('click', function () {
+    // Get the content from the .context element
+    const contextText = contextElement.textContent;
+
+    // Speak the content
+    speakContent(contextText);
+});
